1835644f9953f207adeaf59564e4b9ae641050c3
Simon Belak <simon.belak@gmail.com>
2016-05-03 15:35:42 +0200

    Add box-cox

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.github.brandtg/stl-java "0.1.1"]
-                 [com.github.davidmoten/fastdtw "0.1"]])
+                 [com.github.davidmoten/fastdtw "0.1"]
+                 [net.mikera/core.matrix "0.51.0"]])
--- src/tide/core.clj
+++ src/tide/core.clj
@@ -1,24 +1,51 @@
 (ns tide.core
+  (:require [clojure.core.matrix.stats :refer [mean sd]])
   (:import com.github.brandtg.stl.StlDecomposition))
 
-(def setters {:inner-loop-passes (memfn setNumberOfInnerLoopPasses n)
-              :robustness-iterations (memfn setNumberOfRobustnessIterations n)
-              :trend-bandwidth (memfn setTrendComponentBandwidth bw)
-              :seasonal-bandwidth (memfn setSeasonalComponentBandwidth bw)
-              :loess-robustness-iterations (memfn setLoessRobustnessIterations n)
-              :periodic? (memfn setPeriodic p?)})
+(defn box-cox
+  [lambda x]
+  (if (zero? lambda)
+    (Math/log x)
+    (/ (dec (Math/pow x lambda)) lambda)))
+
+(defn box-cox-inverse
+  [lambda x]
+  (if (zero? lambda)
+    (Math/exp x)
+    (Math/pow (inc (* lambda x)) (/ lambda))))
+
+(defn guerrero
+  ([xs]
+   (guerrero 2 xs))
+  ([length xs]
+   (let [sections (partition-all length xs)
+         cv (fn [lambda]
+              (let [cvs (for [section sections]
+                          (/ (sd section) 
+                             (Math/pow (mean section) (- 1 lambda))))]
+                (/ (sd cvs) 
+                   (mean cvs))))]
+     (apply min-key cv (range 0 1 0.1)))))
+
+(def ^:private setters
+  {:inner-loop-passes (memfn setNumberOfInnerLoopPasses n)
+   :robustness-iterations (memfn setNumberOfRobustnessIterations n)
+   :trend-bandwidth (memfn setTrendComponentBandwidth bw)
+   :seasonal-bandwidth (memfn setSeasonalComponentBandwidth bw)
+   :loess-robustness-iterations (memfn setLoessRobustnessIterations n)
+   :periodic? (memfn setPeriodic periodic?)})
 
 (defn decompose
   ([period ts]
    (decompose period {} ts))
   ([period opts ts]
    (let [xs (map first ts)
-         ys (map second ts)
-         preprocess (if (= (:decomposition opts) :multiplicative)
-                      (partial map #(Math/log %))
+         ys (map second ts)                   
+         preprocess (if-let [transform (:transform opts)]
+                      (partial map transform)
                       identity)
-         postprocess (if (= (:decomposition opts) :multiplicative)
-                       (partial map #(Math/exp %))
+         postprocess (if-let [transform (:reverse-transform opts)]
+                       (partial map transform)
                        vec)
          decomposer (StlDecomposition. period)
          _ (reduce-kv (fn [config k v]
@@ -34,7 +61,3 @@
       :reminder (postprocess (.getRemainder decomposition))
       :xs xs
       :ys ys})))
-
-;; TODO
-;;
-;; Box-Cox
